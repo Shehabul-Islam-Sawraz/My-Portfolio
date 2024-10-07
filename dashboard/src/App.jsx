9316989224d2b504074480c5426a07cfc6b5bc84
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2024-10-07 23:31:26 +0600

    Initial Config Done

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,34 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' Component={HomePage} />
+          <Route path='/login' Component={Login} />
+          <Route path='/password/forgot' Component={ForgotPassword} />
+          <Route path='/password/reset/:token' Component={ResetPassword} />
+          <Route path='/skills' Component={Skills} />
+          <Route path='/timeline' Component={Timeline} />
+          <Route path='/projects' Component={Projects} />
+          <Route path='/project/details/:id' Component={ViewProject} />
+          <Route path='/project/update/:id' Component={UpdateProject} />
+          {/* <Route path='*' element={<h1>404 Page Not Found</h1>} /> */}
+          <Route path='*' element={<Navigate to={"/"} replace />} />
+        </Routes>
+        <ToastContainer position="bottom-right" theme="dark" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   )
 }
 
